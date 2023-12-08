383c8cf37778e033ed3ae5d05b3e5f683d862df9
Kairixir <kairixir@protonmail.com>
2023-12-08 11:28:54 +0100

    fix: fix script to get input data form puzzles

--- get-input.sh
+++ get-input.sh
@@ -5,22 +5,26 @@ if (( $# != 1 )); then
     exit 1
 fi
 
-if ! [[ $1 =~ ^[0-9]+$ ]]; then
-    >&2 echo "Input is not in correct format\nUse format [0-9]{2}"
+if ! [[ $1 =~ ^[1-9]?[0-9]$ ]]; then
+    >&2 echo "Input is not in correct format\nUse format [1-9]?[0-9]"
     exit 1
 fi
 
 source "config.get-input"
 
-mkdir $1
+folder=$(printf '%02d' $1)
 
-curl 'https://adventofcode.com/2023/day/7/input' \
+mkdir $folder
+
+curl "https://adventofcode.com/2023/day/$1/input" \
   -H 'authority: adventofcode.com' \
   -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8' \
   -H 'accept-language: en-GB,en;q=0.9' \
   -H 'cache-control: no-cache' \
   -H "cookie: session=${COOKIE_SESSION}" \
-  --compressed >> $1/input.txt
+  --compressed >> $folder/input.txt
 
-cp $1/input.txt $1/part1-solution.txt
-cp $1/input.txt $1/part2-solution.txt
+
+
+cp $folder/input.txt $folder/part1-solution.txt
+cp $folder/input.txt $folder/part2-solution.txt
