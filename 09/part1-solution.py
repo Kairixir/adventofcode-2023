6225b9cfe3bba12480a5b5d5a9f3be312f40dd4f
Kairixir <kairixir@protonmail.com>
2023-12-10 12:25:19 +0100

    feat: add solution to task 09 part 01 [Python]

--- 09/part1-solution.py
+++ 09/part1-solution.py
@@ -1,35 +1,49 @@
-import re
+import numpy as np
 
 FILE = "part1-solution.txt"
 # FILE = "part1-test.txt"
 
 
-class Node:
-    def __init__(self, val):
-        self.val = val
-        self.parent = None
-
-    def add_child(self, child: Node, is_left_child: bool):
-        if is_left_child:
-            self.l = child
-        self.r = child
-        child.parent = self
-
 class PuzzleInverseTree:
-    def __init__(self, leaves: List[Node]):
+    def __init__(self, leaves):
         self.leaves = leaves
+        self.nodes = None
 
     def build_tree(self):
-        for leaf in enumerate(self.leaves):
-            leaf
+        nodes = [self.leaves]
+        for i in range(len(nodes[0]) - 1, 0, -1):
+            nodes.append(np.zeros(i))
+            for j in range(len(nodes[0]) - i):
+                nodes[j + 1][i - 1] = nodes[j][i] - nodes[j][i - 1]
+            if not nodes[len(nodes) - 1].any():
+                break
+        self.nodes = nodes
 
+    def print_tree(self) -> None:
+        if self.nodes is None:
+            print("Tree is not built")
+            exit(1)
+        print(self.nodes)
+        for i in range(len(self.nodes)):
+            print(self.nodes[i])
+
+    def next_val(self):
+        total = 0
+        for level in self.nodes:
+            total += level[-1]
+        return total
 
 
 def process_line(line: str):
-    nums = map(int, line.strip().split(" "))
-    rows = []
-    for num in nums:
-        
+    nums = np.array(list(map(int, line.strip().split(" "))))
+    pit = PuzzleInverseTree(nums)
+    pit.build_tree()
+    return pit.next_val()
+
 
 with open(FILE) as f:
+    total = 0
     for line in f.readlines():
+        total += process_line(line)
+
+print(total)
